6a26e1ff9df636018b216587f5d82345cd2062bc
chowafu <andre_nazareth@ymail.com>
2023-08-10 13:03:44 +0800

    [Add] Logout audit listener

--- app/Listeners/LogSuccessfulLogout.php
+++ app/Listeners/LogSuccessfulLogout.php
@@ -3,8 +3,8 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Logout;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LogSuccessfulLogout
 {
@@ -21,6 +21,17 @@ class LogSuccessfulLogout
      */
     public function handle(Logout $event): void
     {
-        //
+        // Get User ID
+        $userId = $event->user->id;
+        // Get Logout Datetime
+        $logout_datetime = Carbon::now();
+
+        // Insert log into `audits` table
+        DB::table('audits')->insert([
+            'user_id' => $userId,
+            'action_type' => 'Logout',
+            'action_details' => 'Logged out',
+            'action_date' => $logout_datetime
+        ]);
     }
 }
