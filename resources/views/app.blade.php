8ac1d95f3d9bbad401debd5ad9b89ad87b2aac1a
chowafu <andre_nazareth@ymail.com>
2023-08-15 11:53:58 +0800

    [Update] Removed unused script tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,5 @@
         <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-        <script>
-            
-        </script>
     </body>
 </html>
