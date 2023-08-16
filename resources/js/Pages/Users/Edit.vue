b4079e4df9bec2749f1df50adca4cbdea9f37878
External <129664957+externalip@users.noreply.github.com>
2023-08-16 16:47:38 +0800

    Modified the UI of Create and Edit User

--- resources/js/Pages/Users/Edit.vue
+++ resources/js/Pages/Users/Edit.vue
@@ -1,97 +1,147 @@
 <template>
     <AppLayout title="Edit User">
         <form @submit.prevent="updateAccount">
-            <div class="flex items-center justify-center h-screen">
-                <div class="max-w-3xl mx-auto p-6">
-                    <div class="grid grid-cols-2 gap-4">
-                        <!-- Account Details -->
-                        <div class="col-span-2 md:col-span-1">
-                            <h3 class="text-lg font-semibold mb-2">Account Details</h3>
-                            <div class="space-y-4">
-                                <div>
-                                    <label for="username" class="block mb-1">Username</label>
-                                    <input v-model="form.username" type="text" id="username"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.username" />
-                                </div>
-                                <div>
-                                    <label for="password" class="block mb-1">Password</label>
-                                    <input v-model="form.password" type="password" id="password"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.password" />
-                                </div>
-                                <div>
-                                    <label for="password_confirmation" class="block mb-1">Confirm Password</label>
-                                    <input v-model="form.password_confirmation" type="password" id="password_confirmation"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
-                                </div>
+            <div class=" lg:mx-20 rounded-lg grid grid-row-3 gap-5">
+                <!-- Employee Info -->
+                <div class="mx-3 border-2 border-gray-300 lg:p-10 p-5 rounded-2xl">
+                    <h3 class="text-2xl font-semibold mb-2 md:text-left text-center">
+                        Employee Info
+                    </h3>
+
+                    <!-- INPUTS -->
+                    <div class="space-y-4">
+                        <!-- FIRST NAME & LAST NAME -->
+                        <div class="grid grid-cols-2">
+                            <!-- First Name -->
+                            <div class="col-span-1 p-1">
+                                <label for="first_name" class="block mb-1">First Name</label>
+                                <input v-model="form.first_name" type="text" id="first_name"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.first_name" />
+                            </div>
+
+                            <!-- Last Name -->
+                            <div class="col-span-1 p-1">
+                                <label for="last_name" class="block mb-1">Last Name</label>
+                                <input v-model="form.last_name" type="text" id="last_name"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.last_name" />
                             </div>
                         </div>
 
-                        <!-- Employee Details -->
-                        <div class="col-span-2 md:col-span-1">
-                            <h3 class="text-lg font-semibold mb-2">Employee Details</h3>
-                            <div class="space-y-4">
-                                <div>
-                                    <label for="first_name" class="block mb-1">First Name</label>
-                                    <input v-model="form.first_name" type="text" id="first_name"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.first_name" />
-                                </div>
-                                <div>
-                                    <label for="last_name" class="block mb-1">Last Name</label>
-                                    <input v-model="form.last_name" type="text" id="last_name"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.last_name" />
-                                </div>
-                                <div>
-                                    <label for="gender" class="block mb-1">Gender</label>
-                                    <select v-model="form.gender" type="text" id="gender" class="w-full rounded-lg p-2">
-                                        <option value="" disabled>Gender</option>
-                                        <option value="Male">Male</option>
-                                        <option value="Female">Female</option>
-                                        <option value="Others">Others</option>
-                                    </select>
-                                    <InputError class="mt-2" :message="form.errors.gender" />
-                                </div>
-                                <div>
-                                    <label for="date_of_birth" class="block mb-1">Date of Birth</label>
-                                    <input v-model="form.date_of_birth" type="date" id="date_of_birth"
-                                        class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.date_of_birth" />
-                                </div>
-                                <div>
-                                    <label for="contact" class="block mb-1">Contact</label>
-                                    <input v-model="form.contact" type="text" id="contact" class="w-full rounded-lg p-2"
-                                        @keypress="onlyNumbers($event)" />
-                                    <InputError class="mt-2" :message="form.errors.contact" />
-                                </div>
-                                <div>
-                                    <label for="shift" class="block mb-1">Shift</label>
-                                    <input v-model="form.shift" type="text" id="shift" class="w-full rounded-lg p-2" />
-                                    <InputError class="mt-2" :message="form.errors.shift" />
-                                </div>
-                                <div>
-                                    <label for="role_id" class="block mb-1">Role</label>
-                                    <select v-model="form.role_id" type="text" id="role_id" class="w-full rounded-lg p-2">
-                                        <option v-for="role in roles" :key="role.id" :value="role.id">{{ role.position
-                                        }}
-                                        </option>
-                                        <InputError class="mt-2" :message="form.errors.role_id" />
-                                    </select>
-                                </div>
+                        <!-- DOB, CONTACT, SHIFT, GENDER -->
+                        <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+                            <!-- DATE OF BIRTH -->
+                            <div class="p-1">
+                                <label for="date_of_birth" class="block mb-1">Date of Birth</label>
+                                <input v-model="form.date_of_birth" type="date" id="date_of_birth"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.date_of_birth" />
                             </div>
-                        </div>
-                    </div>
 
-                    <div class="flex justify-center mt-6">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">Update
-                            Account</button>
+                            <!-- CONTACT -->
+                            <div class="p-1">
+                                <label for="contact" class="block mb-1">Contact</label>
+                                <input v-model="form.contact" type="text" id="contact"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.contact" />
+                            </div>
+
+                            <!-- SHIFT -->
+                            <div class=" p-1">
+                                <label for="shift" class="block mb-1">Shift</label>
+                                <select v-model="form.shift" type="text" id="shift"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2">
+                                    <option value="0">None</option>
+                                    <option value="1st">1st</option>
+                                    <option value="2nd">2nd</option>
+                                    <option value="3rd">3rd</option>
+                                </select>
+                                <InputError class="mt-2" :message="form.errors.shift" />
+                            </div>
+
+                            <!-- GENDER -->
+                            <div class="p-1">
+                                <label for="gender" class="block mb-1">Gender</label>
+                                <select v-model="form.gender" type="text" id="gender"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2">
+                                    <option value="" disabled>Gender</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                                <InputError class="mt-2" :message="form.errors.gender" />
+                            </div>
+
+
+                        </div>
                     </div>
                 </div>
+
+                <!-- Account Details -->
+                <div class="mx-3 border-2 border-gray-300 lg:p-10 p-5 rounded-2xl">
+                    <h3 class="text-2xl font-semibold mb-2 md:text-left text-center">
+                        Account Details
+                    </h3>
+
+                    <!-- INPUTS -->
+                    <div class="space-y-4">
+                        <div class="grid lg:grid-cols-2 md:grid-cols-2">
+                            <!-- USERNAME -->
+                            <div class="p-1">
+                                <label for="username" class="block mb-1">Username</label>
+                                <input v-model="form.username" type="text" id="username"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.username" />
+                            </div>
+
+                            <!-- PASSWORD -->
+                            <div class="p-1">
+                                <label for="password" class="block mb-1">Password</label>
+                                <input v-model="form.password" type="password" id="password"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.password" />
+                            </div>
+
+                        </div>
+
+                        <div class="grid lg:grid-cols-2 md:grid-cols-2">
+                            <!-- CONFIRM PASSWORD -->
+                            <div class="p-1">
+                                <label for="password_confirmation" class="block mb-1">Confirm Password</label>
+                                <input v-model="form.password_confirmation" type="password" id="password_confirmation"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2" />
+                                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                            </div>
+                            <!-- ROLE -->
+                            <div class="col-span-0 p-1">
+                                <label for="role_id" class="block mb-1">Role</label>
+                                <select v-model="form.role_id" type="text" id="role_id"
+                                    class="w-full rounded-lg border-2 border-gray-300 p-2">
+                                    <option v-for="role in roles" :key="role.id" :value="role.id">{{ role.position }}
+                                    </option>
+                                    <InputError class="mt-2" :message="form.errors.role_id" />
+                                </select>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <!-- BUTTONS -->
+                <div class="flex justify-center p-10">
+                    <Link :href="route('users')" type="button"
+                        class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    Back</Link>
+                    <button type="submit"
+                        class="text-white bg-[#3C5393] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create
+                        Account</button>
+                </div>
+
             </div>
         </form>
+
     </AppLayout>
 </template>
 
@@ -99,7 +149,8 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import { ref, defineProps } from 'vue';
-import { useForm } from "@inertiajs/vue3";
+import { useForm, Link } from "@inertiajs/vue3";
+
 const { user, roles } = defineProps(['user', 'roles']);
 const form = useForm({
     username: user.username,
@@ -120,14 +171,40 @@ const onlyNumbers = (event) => {
         event.preventDefault();
     }
 };
-
-const updateAccount = () => {
-    form.put(route("users.update", user.id), {
-        preserveScroll: true,
-
-        onFinish: () => {
-
-        },
-    });
+const updateAccount = async () => {
+    try {
+        const response = await axios.put(route('users.update', user.id), form);
+        console.log(response.data.status);
+        if (response.data.status === 'success') {
+            await Swal.fire({
+                title: 'Success!',
+                text: 'User has been updated!',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+            window.location.href = route('users');
+        } else {
+             const errors = response.data.errors;
+            Object.keys(errors).forEach(field => {
+                form.errors[field] = errors[field][0];
+            });
+            console.log(form.errors);
+            await Swal.fire({
+                title: 'Error!',
+                text: 'Failed to update user.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        await Swal.fire({
+            title: 'Error!',
+            text: 'Failed to update user.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
 };
+
 </script>
