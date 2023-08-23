fe794507368d6c97bab1ca9114988d49b5a7a12e
External <129664957+externalip@users.noreply.github.com>
2023-08-23 16:31:11 +0800

    User edit unique rule

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -151,7 +151,7 @@ class UserController extends Controller
     {
         $input = $request->all();
         $validator = Validator::make($input, [
-            'username' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'string', 'max:255'],
