e5504471113c36c40f0afcac95ff87dcba3ffb1f
External <129664957+externalip@users.noreply.github.com>
2023-08-30 14:36:04 +0800

    Fixed Pagination Next button

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -2,26 +2,27 @@
     <div class="flex items-center justify-between p-3 bg-white border-t border-gray-200 sm:px-6">
         <!-- Previous Button (hidden on mobile) -->
         <button @click="gotoPage(data.current_page - 1)" :class="['pagination-button', 'hidden', 'sm:block', { 'disabled': data.current_page <= 1 }]">
-                    Previous
-        </button>
+                        Previous
+            </button>
 
         <div class="flex items-center justify-center flex-1">
             <!-- Flex layout added -->
             <nav class="relative z-0 inline-flex shadow-sm">
                 <!-- Numbered Pages -->
                 <template v-for="num in visiblePages" :key="num">
-                        <button @click="gotoPage(num)"
-                            :class="['pagination-button', num === data.current_page ? 'bg-blue-500 text-white' : 'text-gray-700 hover:text-gray-500']">
-                            {{ num }}
-                        </button>
-                </template>
+                            <button @click="gotoPage(num)"
+                                :class="['pagination-button', num === data.current_page ? 'bg-blue-500 text-white' : 'text-gray-700 hover:text-gray-500']">
+                                {{ num }}
+                            </button>
+</template>
             </nav>
         </div>
 
         <!-- Next Button (hidden on mobile) -->
-        <button @click="gotoPage(data.current_page + 1)"
-                :class="['pagination-button', 'hidden', 'sm:block', { 'disabled': data.current_page >= data.last_page }]">
-                Next
+       <button @click="gotoPage(data.current_page + 1)"
+            :class="['pagination-button', 'hidden', 'sm:block', { 'disabled': data.current_page >= data.last_page }]"
+            :disabled="data.current_page >= data.last_page">
+             Next
         </button>
     </div>
 </template>
