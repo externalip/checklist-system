de651b8d784964ceaab02283d54fb59dc4d8fc00
chowafu <chowafu@users.noreply.github.com>
2023-09-03 10:06:02 +0000

    Chore: Fix styling

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -113,7 +113,7 @@ class ResponseController extends Controller
         $timestamp = now()->format('ymdHis');
         $userId = auth()->user()->id;
 
-        $responseNo = $timestamp . '-' . $userId;
+        $responseNo = $timestamp.'-'.$userId;
 
         return $responseNo;
     }
