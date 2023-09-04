c4a359f5cdb066fae0ebc831c6a28ab2cd79c4bc
spykeyy <carlopalo04@gmail.com>
2023-09-04 08:40:35 +0800

    changed checksheet manager color

--- resources/js/Pages/Create-Checklist/Edit/Edit.vue
+++ resources/js/Pages/Create-Checklist/Edit/Edit.vue
@@ -1,8 +1,8 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue'
-import Filter from './Filter.vue';
-import Pagination from '@/Shared/Pagination.vue'
-import { Link, router } from '@inertiajs/vue3';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Filter from "./Filter.vue";
+import Pagination from "@/Shared/Pagination.vue";
+import { Link, router } from "@inertiajs/vue3";
 
 const props = defineProps({
     forms: Object,
@@ -10,30 +10,30 @@ const props = defineProps({
 const confirmDelete = async (formID) => {
     try {
         const confirmation = await Swal.fire({
-            title: 'Confirm Delete',
-            text: 'Are you sure you want to delete this form?',
-            icon: 'warning',
+            title: "Confirm Delete",
+            text: "Are you sure you want to delete this form?",
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Yes, delete it!',
-            cancelButtonText: 'Cancel'
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "Cancel",
         });
 
         if (!confirmation.isConfirmed) {
             return;
         }
 
-        const deleteLink = route('checksheet.delete');
+        const deleteLink = route("checksheet.delete");
         const result = await axios.delete(deleteLink, { data: { id: formID } });
 
         if (result.status == 200) {
             await Swal.fire({
-                title: 'Deleted!',
-                text: 'The form has been deleted.',
-                icon: 'success',
+                title: "Deleted!",
+                text: "The form has been deleted.",
+                icon: "success",
                 timer: 1500,
-                showConfirmButton: true
+                showConfirmButton: true,
             });
 
             setTimeout(() => {
@@ -41,20 +41,20 @@ const confirmDelete = async (formID) => {
             }, 500);
         } else {
             await Swal.fire({
-                title: 'Error!',
-                text: 'An error occurred while deleting the form.',
-                icon: 'error',
+                title: "Error!",
+                text: "An error occurred while deleting the form.",
+                icon: "error",
                 showCancelButton: false,
-                confirmButtonText: 'OK'
+                confirmButtonText: "OK",
             });
         }
     } catch (error) {
         await Swal.fire({
-            title: 'Error!',
+            title: "Error!",
             text: `An error occurred while deleting the form. ${error}`,
-            icon: 'error',
+            icon: "error",
             showCancelButton: false,
-            confirmButtonText: 'OK'
+            confirmButtonText: "OK",
         });
     }
 };
@@ -68,14 +68,15 @@ const confirmDelete = async (formID) => {
 
             <section id="checksheet-registered-table">
                 <section id="checksheet-table-section">
-
                     <div class="relative overflow-x-auto">
-
                         <!-- Table -->
-                        <table class="rounded-lg w-full text-sm text-center text-gray-500 dark:text-gray-400">
-
+                        <table
+                            class="rounded-lg w-full text-sm text-center text-gray-500 dark:text-gray-400"
+                        >
                             <!-- Table Headers -->
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-white uppercase bg-[--blue] dark:bg-gray-700 dark:text-gray-400"
+                            >
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
                                         Checksheet ID
@@ -100,10 +101,14 @@ const confirmDelete = async (formID) => {
 
                             <!-- Table Content -->
                             <tbody>
-                                <tr v-for="form in forms.data"
-                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 transition ease-in-out duration-200">
-                                    <td scope="row"
-                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <tr
+                                    v-for="form in forms.data"
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 transition ease-in-out duration-200"
+                                >
+                                    <td
+                                        scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                    >
                                         {{ form.id }}
                                     </td>
                                     <td class="px-6 py-4">
@@ -121,57 +126,84 @@ const confirmDelete = async (formID) => {
 
                                     <!-- Action Buttons -->
                                     <td class="px-6 flex justify-center mx-10">
-
                                         <!-- Edit Button -->
-                                        <Link type="button" :href="route('checksheet.edit', form.id)"
-                                            class="hover:bg-gray-200 transition duration-200 ease-in-out p-4">
-                                        <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2">
-                                                <path
-                                                    d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z" />
-                                                <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                            </g>
-                                        </svg>
+                                        <Link
+                                            type="button"
+                                            :href="
+                                                route(
+                                                    'checksheet.edit',
+                                                    form.id
+                                                )
+                                            "
+                                            class="hover:bg-gray-200 transition duration-200 ease-in-out p-4"
+                                        >
+                                            <svg
+                                                class="w-5 h-5 text-gray-800 dark:text-white"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 20 20"
+                                            >
+                                                <g
+                                                    stroke="currentColor"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                >
+                                                    <path
+                                                        d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z"
+                                                    />
+                                                    <path
+                                                        d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                                                    />
+                                                </g>
+                                            </svg>
                                         </Link>
 
                                         <!-- Delete Button -->
-                                        <button type="button" @click="confirmDelete(form.id)"
-                                            class="hover:bg-gray-200 transition duration-200 ease-in-out p-4">
-                                            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        <button
+                                            type="button"
+                                            @click="confirmDelete(form.id)"
+                                            class="hover:bg-gray-200 transition duration-200 ease-in-out p-4"
+                                        >
+                                            <svg
+                                                class="w-5 h-5 text-gray-800 dark:text-white"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 18 20"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
                                                     stroke-width="2"
-                                                    d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z" />
+                                                    d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                                                />
                                             </svg>
                                         </button>
                                     </td>
                                 </tr>
                             </tbody>
-
                         </table>
                     </div>
-
                 </section>
-
             </section>
             <Pagination :data="forms" />
         </div>
-
     </AppLayout>
 </template>
 
 <script>
 export default {
-    name: 'checksheet',
+    name: "checksheet",
     methods: {
         callEditor(form_id, form_config) {
-            router.get('checksheet/editor', { form_id, form_config });
+            router.get("checksheet/editor", { form_id, form_config });
         },
         callCreator() {
-            router.get('/generate');
-        }
+            router.get("/generate");
+        },
     },
-}
+};
 </script>
--- resources/js/Pages/Create-Checklist/Edit/Filter.vue
+++ resources/js/Pages/Create-Checklist/Edit/Filter.vue
@@ -3,26 +3,75 @@
         <div class="model-filter-header">
             <h4>Filter by:</h4>
         </div>
-        <section id="checksheet-filters" class="mb-5 grid lg:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-3">
+        <section
+            id="checksheet-filters"
+            class="mb-5 grid lg:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-3"
+        >
             <div id="checksheet-filter-name-search">
                 <form>
-                    <label for="filter-name-search" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Search by Checksheet Name</label>
+                    <label
+                        for="filter-name-search"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >Search by Checksheet Name</label
+                    >
                     <div class="relative">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
+                        <div
+                            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+                        >
+                            <svg
+                                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                />
+                            </svg>
                         </div>
-                        <input v-model="name" type="search" id="default-search-name" class="block w-full p-2.5 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search Checksheet Name" required>
+                        <input
+                            v-model="name"
+                            type="search"
+                            id="default-search-name"
+                            class="block w-full p-2.5 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Search Checksheet Name"
+                            required
+                        />
                     </div>
                 </form>
             </div>
             <div id="checksheet-add-btn">
-                <label for="filter-user-select" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Click to Add Checksheet</label>
-                <Link type="button" :href="route('generate')" class="w-60 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-12 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center gap-3 justify-center"> Create Form <svg class="w-3 h-3 text-gray-100 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
-              </svg>
+                <label
+                    for="filter-user-select"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Click to Add Checksheet</label
+                >
+                <Link
+                    type="button"
+                    :href="route('generate')"
+                    class="w-60 text-white bg-[--blue] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-12 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center gap-3 justify-center"
+                >
+                    Create Form
+                    <svg
+                        class="w-3 h-3 text-gray-100 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 18"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+                        />
+                    </svg>
                 </Link>
             </div>
         </section>
@@ -33,18 +82,18 @@
 import { Link, router } from "@inertiajs/vue3";
 import { watch, ref } from "vue";
 
-let name = ref('');
-let props = defineProps({filters: Object});
+let name = ref("");
+let props = defineProps({ filters: Object });
 const updateFilters = (name) => {
-    const filter = { ...props.filters};
-    if(name){
+    const filter = { ...props.filters };
+    if (name) {
         filter.name = name;
     }
-    router.get(route('checksheet'), filter, {
+    router.get(route("checksheet"), filter, {
         preserveState: true,
         preserveScroll: true,
         replace: true,
-    })
+    });
 };
 watch(name, updateFilters);
 </script>
