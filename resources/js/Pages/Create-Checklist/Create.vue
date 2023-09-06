c0b1d9b6538c9a38955577f377cc482f20079687
External <129664957+externalip@users.noreply.github.com>
2023-09-06 08:46:41 +0800

    Disabled the option choose a type

--- resources/js/Pages/Create-Checklist/Create.vue
+++ resources/js/Pages/Create-Checklist/Create.vue
@@ -145,7 +145,7 @@ import { reactive } from 'vue';
                                                 :name="'v'"
                                                 class="required border-1 border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 required>
-                                                <option selected>
+                                                <option disabled>
                                                     Choose a type
                                                 </option>
                                                 <option :value="'radio'">
