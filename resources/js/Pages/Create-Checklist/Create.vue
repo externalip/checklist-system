56a467c3c4501b180413bd092da29d522a4aa4c8
chowafu <andre_nazareth@ymail.com>
2023-09-06 08:55:20 +0800

    [Update] Disabled Textbox

--- resources/js/Pages/Create-Checklist/Create.vue
+++ resources/js/Pages/Create-Checklist/Create.vue
@@ -183,7 +183,7 @@ const submit = async () => {
                                                     Choose a type
                                                 </option>
                                                 <option :value="'radio'">
-                                                    Radio
+                                                    Multiple Choice
                                                 </option>
                                                 <option :value="'checkbox'">
                                                     Check
@@ -322,10 +322,10 @@ const submit = async () => {
                                             <!-- Text Input Label -->
                                             <div class="relative">
                                                 <input type="text" :name="'text' + key"
-                                                    class="bg-[--disabled] border-[--disabled-outline] block px-2.5 pb-1.5 pt-3 w-5/6 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                                    class="bg-[--disabled] border-[--disabled-outline] block px-2.5 pb-1.5 pt-3 w-5/6 text-sm text-gray-900 rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                                     placeholder=" " disabled />
                                                 <label for="section_name"
-                                                    class="bg-[--disabled] absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Type
+                                                    class="bg-[--disabled] absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Type
                                                     here...</label>
                                             </div>
                                         </div>
