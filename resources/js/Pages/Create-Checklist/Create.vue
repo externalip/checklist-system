9a4935ba82352c7f47cdd7c1d87fd2dfce6653e2
spykeyy <carlopalo04@gmail.com>
2023-09-08 09:48:52 +0800

    fixed styling on and icon on nav

--- resources/js/Pages/Create-Checklist/Create.vue
+++ resources/js/Pages/Create-Checklist/Create.vue
@@ -23,7 +23,7 @@ import { reactive } from 'vue';
         <form @submit.prevent="submit">
         <div class="lg:mx-[25%] mb-40">
             <div class="mx-auto">
-                
+
                 <!-- CHECKSHEET NAME -->
                 <div class="mb-5">
                     <p class="text-xl mb-3">Check Sheet Name</p>
@@ -493,10 +493,10 @@ import { reactive } from 'vue';
             <div class="grid h-full max-w-lg grid-cols-3 mx-auto">
                 <button type="button" @click="redirectBack"
                     class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                    <svg class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                    <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 16 16">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3" />
                     </svg>
                     <span class="sr-only">Back</span>
                 </button>
--- resources/js/Pages/Users/Components/Modal.vue
+++ resources/js/Pages/Users/Components/Modal.vue
@@ -1,32 +1,32 @@
 <template>
-    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-50">
-        <div class="relative w-full max-w-2xl max-h-full">
+    <div v-if="showModal" class="text-[--blue] fixed inset-0 flex items-center justify-center z-50">
+        <div class="relative w-full max-w-md max-h-full">
             <!-- Modal content -->
-            <div class="p-6 md:px-20 relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="p-6 md:px-5 relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <!-- Modal header -->
                 <div class="flex items-start pb-3 justify-between rounded-t dark:border-gray-600">
-                    <h3 class="text-3xl font-semibold text-gray-900 dark:text-white">
+                    <h3 class="text-3xl text-center md:text-left font-semibold dark:text-white">
                         Add Role
                     </h3>
                 </div>
                 <form @submit.prevent="submit">
                     <div class="mb-2">
-                        <label for="role_position" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role
+                        <label for="role_position" class="block mb-2 text-sm font-medium dark:text-white">Role
                             Position</label>
                         <input type="text" id="role_position"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Role Position..." v-model="Roles.position" required>
                     </div>
                     <div class="mb-2">
-                            <label for="add_role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role Description</label>
+                            <label for="add_role" class="block mb-2 text-sm font-medium dark:text-white">Role Description</label>
                             <input type="text" id="role_description"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Role Description..." v-model="Roles.description" required>
                         </div>
                     <div class="flex justify-end mt-4">
                         <button type="submit"
-                            class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">Add Role</button>
-                        <button @click="closeModal" type="button" class="ml-2 border rounded-lg px-4 py-2">Cancel</button>
+                            class="bg-[--blue] duration-200 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">Add Role</button>
+                        <button @click="closeModal" type="button" class="duration-200 hover:bg-gray-100 ml-2 border rounded-lg px-4 py-2">Cancel</button>
                     </div>
                 </form>
             </div>
