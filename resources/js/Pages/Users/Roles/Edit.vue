30d54b556cf2d024da1e66b39610ef9af56b6259
spykeyy <carlopalo04@gmail.com>
2023-09-15 13:55:57 +0800

    changed multiselect and role layout

--- resources/js/Pages/Users/Roles/Edit.vue
+++ resources/js/Pages/Users/Roles/Edit.vue
@@ -1,11 +1,17 @@
 <template>
     <AppLayout title="Update Role">
         <form @submit.prevent="updateRole(role.id)">
-            <div class=" lg:mx-20 rounded-lg grid grid-row-3 gap-5 text-[--blue]">
+            <div
+                class="lg:mx-[25%] rounded-lg grid grid-row-3 gap-5 text-[--blue]"
+            >
                 <!-- Employee Info -->
-                <div class="mx-3 border-2 border-gray-300 lg:p-10 p-5 rounded-2xl">
-                    <h3 class="text-2xl font-semibold mb-2 md:text-left text-center">
-                        Roles
+                <div
+                    class="mx-3 border-2 border-gray-300 lg:p-10 p-5 rounded-2xl"
+                >
+                    <h3
+                        class="text-2xl font-semibold mb-2 md:text-left text-center"
+                    >
+                        Update Role
                     </h3>
 
                     <!-- INPUTS -->
@@ -14,26 +20,64 @@
                         <div class="grid md:grid-cols-2 grid-cols-1">
                             <!-- Role Name -->
                             <div class="col-span-1 p-1">
-                                <label for="role_name" class="block mb-1">Role Name</label>
-                                <input v-model="form.name" type="text" id="role_name" class="text-gray-900 w-full rounded-lg border-2 border-gray-300 p-2" />
-                                <InputError class="mt-2" :message="form.errors.name" />
+                                <label for="role_name" class="block mb-1"
+                                    >Role Name</label
+                                >
+                                <input
+                                    v-model="form.name"
+                                    type="text"
+                                    id="role_name"
+                                    class="text-sm text-gray-900 w-full rounded-lg border-1 border-gray-300 p-2"
+                                />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.name"
+                                />
                             </div>
 
                             <!-- Last Name -->
                             <div class="col-span-1 p-1">
-                                <label for="role_description" class="block mb-1">Role Description</label>
-                                <input v-model="form.description" type="text" id="role_description" class="text-gray-900 w-full rounded-lg border-2 border-gray-300 p-2" />
-                                <InputError class="mt-2" :message="form.errors.description" />
+                                <label for="role_description" class="block mb-1"
+                                    >Role Description</label
+                                >
+                                <input
+                                    v-model="form.description"
+                                    type="text"
+                                    id="role_description"
+                                    class="text-sm text-gray-900 w-full rounded-lg border-1 border-gray-300 p-2"
+                                />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.description"
+                                />
                             </div>
                         </div>
 
                         <!-- DOB, CONTACT, SHIFT, GENDER -->
-                        <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-                            <!-- DATE OF BIRTH -->
-                            <div >
-                                <multiselect v-model="selectedPermission" :options="permission_options" :multiple="true" :close-on-select="false" :clear-on-select="false" placeholder="Select an option" label="name" track-by="id" select-label="id" groupSelect="true" @select="onOptionSelect"></multiselect>
-                                <span v-if="!selectedPermission.length" class="text-red-600 text-sm">Please select at least one option.</span>
+                        <div>
+                            <label for="role_access" class="block mb-1"
+                                >This role can only access:</label
+                            >
 
+                            <div>
+                                <multiselect
+                                    v-model="selectedPermission"
+                                    :options="permission_options"
+                                    :multiple="true"
+                                    :close-on-select="false"
+                                    :clear-on-select="false"
+                                    placeholder="Select an option"
+                                    label="name"
+                                    track-by="id"
+                                    select-label="id"
+                                    groupSelect="true"
+                                    @select="onOptionSelect"
+                                ></multiselect>
+                                <span
+                                    v-if="!selectedPermission.length"
+                                    class="text-red-600 text-sm"
+                                    >Please select at least one option.</span
+                                >
                             </div>
                         </div>
                     </div>
@@ -41,24 +85,86 @@
 
                 <!-- BUTTONS -->
                 <div class="flex justify-center p-10">
-                    <button type="submit" class="duration-200 text-white bg-[#3C5393] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update
-                            Role</button>
-                    <Link :href="route('roles.index')" type="button" class="duration-200 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> Back
+                    <button
+                        type="submit"
+                        class="duration-200 text-white bg-[#3C5393] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    >
+                        Update Role
+                    </button>
+                    <Link
+                        :href="route('roles.index')"
+                        type="button"
+                        class="duration-200 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                        Back
                     </Link>
                 </div>
-
             </div>
         </form>
     </AppLayout>
 </template>
 
+<style>
+.multiselect__tag {
+    color: rgb(48, 48, 48);
+    background-color: rgba(255, 255, 255, 255);
+    border: 1px solid rgba(111, 118, 221, 0.726);
+    font-size: 01rem;
+}
+.multiselect__tag:hover {
+    background-color: var(--blue);
+    transition: all 0.2s ease-in-out;
+    color: aliceblue;
+}
+
+.multiselect__option--disabled {
+    background: rgb(209, 202, 209) !important;
+    color: rgba(var(--vs-primary), 1);
+    font-style: italic;
+}
+
+.multiselect__option--highlight {
+    background-color: var(--blue) !important;
+    color: var(--input) !important;
+    font-size: 0.9rem;
+    height: 1rem;
+    transition: all 0.2s ease-in-out;
+}
+
+.multiselect__content {
+    background: rgb(255, 255, 255);
+    color: #444 !important;
+    font-size: 0.9rem;
+}
+.multiselect__single {
+    height: 0.8rem;
+    font-size: 1rem;
+    color: #444 !important;
+}
+.multiselect__tag {
+    position: relative;
+    display: inline-block;
+    padding: 0.35rem 1.625rem 0.35rem 0.625rem;
+
+    margin-right: 0.625rem;
+
+    line-height: 1;
+
+    margin-bottom: 0.5rem;
+}
+</style>
+
 <script setup>
-import { defineProps, ref } from 'vue';
-import { useForm, Link } from '@inertiajs/vue3';
-import Multiselect from 'vue-multiselect';
-import AppLayout from '@/Layouts/AppLayout.vue';
-import 'vue-multiselect/dist/vue-multiselect.css';
-const { permissions, role } = defineProps({ permissions: Array, role: Array, permission_options: Array });
+import { defineProps, ref } from "vue";
+import { useForm, Link } from "@inertiajs/vue3";
+import Multiselect from "vue-multiselect";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import "vue-multiselect/dist/vue-multiselect.css";
+const { permissions, role } = defineProps({
+    permissions: Array,
+    role: Array,
+    permission_options: Array,
+});
 
 const form = useForm({
     name: role.name,
@@ -68,25 +174,25 @@ const form = useForm({
 const selectedPermission = ref(permissions);
 const updateRole = async (id) => {
     try {
-        const response = await axios.put(route('roles.update', id), {
+        const response = await axios.put(route("roles.update", id), {
             name: form.name,
             description: form.description,
             permissions: selectedPermission.value,
         });
         Swal.fire({
-            icon: 'success',
-            title: 'Role Update!',
-            text: 'The role has been successfully updated.',
+            icon: "success",
+            title: "Role Update!",
+            text: "The role has been successfully updated.",
         });
         form.reset();
         window.location.reload(true);
     } catch (error) {
         // Handle error
-        console.error('Error creating role:', error);
+        console.error("Error creating role:", error);
         Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'There was an error creating the role.',
+            icon: "error",
+            title: "Error",
+            text: "There was an error creating the role.",
         });
     }
 };
